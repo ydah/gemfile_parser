df9f4e6bcbb880b696cfe1e7817a8e36a2df6c89
ydah <13041216+ydah@users.noreply.github.com>
2022-06-01 14:13:48 +0900

    bundle gem gemfile_parser

---
+++ lib/gemfile_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gemfile_parser/version"
+
+module GemfileParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gemfile_parser/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GemfileParser
+  VERSION = "0.1.0"
+end
